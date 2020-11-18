c58e0daf00e78cba3864e685980dcf32e7565497
DESKTOP-FF9VO80\KGA_18 <ug4306@gmail.com>
2020-11-18 17:32:49 +0900

    no message

--- Algorithm/10896/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	int A, B;
-	std::cin >> A >> B;
-	if (A < 1 || B > 10000) return 0;
-	std::cout << A + B << "\n" << A - B << "\n" << A * B << "\n" << A / B << "\n" << A % B;
-}
