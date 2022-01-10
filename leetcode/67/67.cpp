dd9b6ee9c9d1f8dd9460f85f56693b793665fb45
egyeom <ug4306@gmail.com>
2022-01-10 18:24:11 +0900

    leetcode 67

---
+++ leetcode/67/67.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans;
+        int carry = 0;
+        int i = a.size() -1; int j = b.size() - 1;
+        while(i >= 0 || j >= 0 || carry == 1)
+        {
+            carry += i >= 0 ? a[i--] - '0' : 0; 
+            carry += j >= 0 ? b[j--] - '0' : 0;
+            ans = char(carry % 2 + '0') + ans;
+            carry /= 2;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    string a = "1001";
+    string b = "1011";
+    Solution s;
+    std::cout << s.addBinary(a,b) <<"\n";
+}
