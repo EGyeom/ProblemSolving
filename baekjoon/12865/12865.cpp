9826a1248d44c6fb9a80b3e99f8a1e9ae878bd40
egyeom <ug4306@gmail.com>
2022-06-24 19:14:26 +0900

    knapsack problem

--- baekjoon/12865/12865.cpp
+++ baekjoon/12865/12865.cpp
@@ -28,15 +28,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[100001];
+int dp[101][100001];
+int W[101];
+int V[101];
 int main()
 {
     int n,k;
     scanf("%d %d", &n, &k);
 
-    for(int i =0; i < n; i++)
+    for(int i =1; i <= n; i++)
     {
-        
+	    scanf("%d", &W[i]);
+	    scanf("%d", &V[i]);
     }
 
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= k; j++)
+        {
+            if(W[i] <= j)
+            {
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
+            }
+            else
+            {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    printf("%d\n", dp[n][k]);
 }
+
