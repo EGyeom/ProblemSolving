2adc7799f26cc479493d6de2809b588993c4ecb4
DESKTOP-HLU2P7C\dmlru <ug4306@gmail.com>
2025-08-07 20:42:23 +0900

    Leetcode solved 141 & reviewed 1394

--- leetcode/1394.cpp
+++ leetcode/1394.cpp
@@ -1,22 +1,45 @@
+// class Solution {
+// public:
+//     int findLucky(vector<int>& arr) {
+
+//         int n = arr.size();
+//         vector<int> count(500,0);
+
+//         for(int num : arr)
+//         {
+//             count[num-1]++;
+//         }
+
+//         for(int i = n-1; i >= 0; i--)
+//         {
+//             if(count[i] == i+1)
+//                 return i+1;
+//         }
+
+//         return -1;
+//     }
+// };
+
+
+//reviewed
 class Solution {
 public:
     int findLucky(vector<int>& arr) {
-
-        int n = arr.size();
         vector<int> count(500,0);
-
-        for(int num : arr)
+        int answer = -1;
+        int n = arr.size();
+        
+        for(int i = 0; i < n; i++)
         {
-            count[num-1]++;
+            count[arr[i]-1]++;
         }
 
-        for(int i = n-1; i >= 0; i--)
+        for(int i = 0; i < n; i++)
         {
-            if(count[i] == i+1)
-                return i+1;
+            if(count[arr[i]-1] == arr[i])
+                answer = max(answer, arr[i]);
         }
 
-        return -1;
+        return answer;
     }
 };
-
