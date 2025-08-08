867e7e9084b3cb25912c911e79314c77a34e3cfa
DESKTOP-HLU2P7C\dmlru <ug4306@gmail.com>
2025-08-08 23:51:39 +0900

    Reviewed 121

--- leetcode/121.cpp
+++ leetcode/121.cpp
@@ -1,19 +1,39 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int profit = 0;
+//         int lowestPrice = prices[0];
+//         for(int i = 1; i < prices.size(); i++)
+//         {
+//             if(prices[i] < lowestPrice)
+//             {
+//                 lowestPrice = prices[i];
+//             }
+//             else if(prices[i] > lowestPrice)
+//             {
+//                 int currentProfit = prices[i] - lowestPrice;
+//                 profit = profit > currentProfit ? profit : currentProfit;
+//             }
+//         }
+//         return profit;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
-        int lowestPrice = prices[0];
-        for(int i = 1; i < prices.size(); i++)
+        int n = prices.size();
+        int minPrice = prices[0];
+        for(int i = 1; i < n ; i++)
         {
-            if(prices[i] < lowestPrice)
+            if(minPrice < prices[i])
             {
-                lowestPrice = prices[i];
+                int currentProfit = prices[i] - minPrice;
+                profit = max(profit, currentProfit);
             }
-            else if(prices[i] > lowestPrice)
-            {
-                int currentProfit = prices[i] - lowestPrice;
-                profit = profit > currentProfit ? profit : currentProfit;
-            }
+
+            minPrice = min(minPrice, prices[i]);
         }
         return profit;
     }
